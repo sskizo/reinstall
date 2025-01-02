4c0f1397ec90df279dea0eb502c3b2793f5358af
sskizo <71433028+sskizo@users.noreply.github.com>
2025-01-02 00:29:12 -0800

    Update reinstall.bat

--- reinstall.bat
+++ reinstall.bat
@@ -2,7 +2,7 @@
 mode con cp select=437 >nul
 setlocal EnableDelayedExpansion
 
-set confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
+set confhome=https://raw.githubusercontent.com/sskizo/reinstall/main
 set confhome_cn=https://jihulab.com/bin456789/reinstall/-/raw/main
 rem set confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
